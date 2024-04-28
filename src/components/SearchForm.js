a68cd1cd9d5bdd6444c252f0979293a4d6257f47
Maria Kamau <maria.kamau@student.moringaschool.com>
2024-04-28 16:01:14 +0300

    Added new components for search form, new item form, and transactions list to the app; updated CSS for layout and styling.

---
+++ src/components/SearchForm.js
@@ -0,0 +1,8 @@
+export default function SearchForm(){
+    return (
+            <form id="search-form">
+                <input id="search" placeholder="Search Transanctions..." type={"text"}/>
+                <button>search</button>
+            </form>
+    )
+}
